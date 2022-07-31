517722143cd557a4003b2a5ec3c691ff8d4a965d
ParagMhrjn <chandra_parag@hotmail.com>
2022-07-31 14:02:01 +0200

    renamed html file

--- js/chapter1.1.js
+++ js/chapter1.1.js
@@ -1,8 +1,7 @@
 // constant
 const Y_AXIS = 1;
 const X_AXIS = 2;
-// var nums =200;
-// var noiseScale = 800;
+
 let synth;
 function setup() {
     var canvas =  createCanvas(windowWidth, windowHeight);
@@ -23,9 +22,7 @@ function setup() {
   
 
     
-
-    // colorMode(HSB, 360, 100, 100, 100);
-    // angleMode(DEGREES);   
+  
 
 }
 
@@ -35,36 +32,10 @@ function setup() {
     setGradient(0 , 0 , width , height , p, b,  Y_AXIS);
     wave();
     
-    // background(0, 0, 90);
-  
-    // push();
-    // translate(width / 2, height / 2);
-  
-    // ellipseMode(RADIUS);
-    // // circle(0, 0, 150);
-    // drawInnerCircles(0, 0, 350, constrain(tan(sin(frameCount) * 90) * 100, 0, 500));
-    // pop();
-  
-    // // noLoop();
+    
   
   }
   
-  // function drawInnerCircles(x, y, r, minD) {
-  //   push();
-  //   translate(x, y);
-  //   for (let angle = 0; angle < 360; angle += 1) {
-  //     let d = map(sin(angle * 3) * sin(frameCount * 5), -1, 1, 0, minD);
-  //     let x = cos(angle) * (r - d / 2);
-  //     let y = sin(angle) * (r - d / 2);
-  //     // fill(0,0,100)
-  //     noFill();
-  //     stroke(0, 0, 0, 10);
-  //     circle(x, y, d / 2);
-  //   }
-  
-  //   pop();
-  // }
-
   function playSynth() {
     userStartAudio();
   
